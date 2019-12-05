b588f0556276ea7384ecbf26471a0ba36b94a4e3
Alice Russell <alice.louise.russell@gmail.com>
2019-12-05 12:45:33 -0500

    initial employee prompts

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const inquirer = require("inquirer");
 const jest = require("jest");
 
-// return inquirer
-//     .prompt([{
-//         type: "input",
-//         name: 
-
-
-
-
-
-
-
-
-//     }])
+return inquirer
+    .prompt([{
+        type: "input",
+        name: "name",
+        message: "What is your name?"
+    },
+    { 
+        type: "number",
+        name: "id",
+        message: "What is your ID number?"
+    },
+    {
+        type: "list",
+        name: "input",
+        message: "What is your role?",
+        choices: ["Manager", "Engineer", "Intern"]
+    }])
